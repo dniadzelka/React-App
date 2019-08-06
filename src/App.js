654e48faee6db62b8bf2e6f2db22e5eae59166f4
Dmitry Nedelko <DmitryNedelko@coherentsolutions.com>
2019-08-06 16:01:27 +0300

    setup https://dniadzelka.github.io/react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-import AppRouter from "./appRouter/AppRouter";
-import Header from "./components/header/Header";
-import {BrowserRouter} from "react-router-dom";
+import AppRouter from './appRouter/AppRouter';
+import Header from './components/header/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<BrowserRouter>
-					<Fragment>
-						<Header></Header>
-						<AppRouter></AppRouter>
-					</Fragment>
-				</BrowserRouter>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="App">
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                    <Fragment>
+                        <Header />
+                        <AppRouter />
+                    </Fragment>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
