4bdf1293e7ca727f0f90ad681b4f854445b72733
Dmitry Nedelko <DmitryNedelko@coherentsolutions.com>
2019-08-07 18:27:05 +0300

    Updates

--- precache-manifest.871db13c0ad76dbaa127b1e40fea455b.js
+++ precache-manifest.871db13c0ad76dbaa127b1e40fea455b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2001a39d58a2a92b69c782bbbfcc1e59",
+    "revision": "4ce686351f7bade35f0af4103a6645a1",
     "url": "/react-app/index.html"
   },
   {
-    "revision": "8c7397146cf6c043a7bf",
-    "url": "/react-app/static/css/main.ade3860e.chunk.css"
+    "revision": "bf20a9b27024e1810761",
+    "url": "/react-app/static/css/main.65dae8a3.chunk.css"
   },
   {
-    "revision": "b33f68e4b3287c0d92b9",
-    "url": "/react-app/static/js/2.37a65954.chunk.js"
+    "revision": "aac310f13df53e5b5ac4",
+    "url": "/react-app/static/js/2.bbd3ad69.chunk.js"
   },
   {
-    "revision": "8c7397146cf6c043a7bf",
-    "url": "/react-app/static/js/main.409fc9c7.chunk.js"
+    "revision": "bf20a9b27024e1810761",
+    "url": "/react-app/static/js/main.99c9e5b1.chunk.js"
   },
   {
     "revision": "9ed022644c6f38b380ac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.c0e8054fdd4a17be214420731c266c17.js"
+  "/react-app/precache-manifest.871db13c0ad76dbaa127b1e40fea455b.js"
 );
 
 self.addEventListener('message', (event) => {
